b26d5676d97a76ff0e9cd793e0619e29709d4e9b
Roman <troschkin.roma@yandex.ru>
2023-09-05 12:10:59 +0300

    another

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-const numberOfFilms = prompt("How many movies do you see?","0");
-
-//console.log(numberOfFilms);
-
 const personalMoviesDB = {
-    count:  numberOfFilms,
+    count:  0,
     movies: {},
     geners: [],
    privat: false
 };
 
-const lastmovies = prompt("One of the last movies","Name of movie");
-let ans1 = lastmovies;
+for(let i=0;i<1;i++)
+{
+    const numberOfFilms = prompt("How many movies do you see?","0");
+    const lastmovies = prompt("One of the last movies","Name of movie");
+    
+    if( lastmovies === true &&(lastmovies.length != 0 || lastmovies.length<=50 ))
+    {
+        personalMoviesDB.movies[0]=lastmovies;
+    }else 
+    {
+        alert("Введите правильно должно быть не ноль и не ботльше 50 символов")
 
-const rate = prompt("Give points rate 0 to 10","0");
-let ans2 = rate;
+        const numberOfFilms = prompt("How many movies do you see?","0");
+        const lastmovies = prompt("One of the last movies","Name of movie");
+        const rate = prompt("Give points rate 0 to 10","0");
+    }
+    
+    
+    personalMoviesDB.count=numberOfFilms;
+    personalMoviesDB.movies[1]=rate;
 
-personalMoviesDB.movies[0]=ans1;
-personalMoviesDB.movies[1]=ans2;
+   
+}
+
+if(personalMoviesDB.count<10) 
+    alert("sefe")
+else if (personalMoviesDB.count>=10 && personalMoviesDB.count<=30)
+alert(1);
+
 
 console.log(personalMoviesDB);
 
