bea217644f9a687b9fab1d9743711482e20b9d38
Roman <troschkin.roma@yandex.ru>
2023-09-05 15:08:32 +0300

    new one

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/**
 const personalMoviesDB = {
     count:  0,
     movies: {},
@@ -33,7 +34,56 @@ if(personalMoviesDB.count<10)
     alert("sefe")
 else if (personalMoviesDB.count>=10 && personalMoviesDB.count<=30)
 alert(1);
+//console.log(personalMoviesDB);
+ */
+
+var array = ['a','e','i','o','u'];
+
+function getCount(str) {
+    
+    let count=0;
+    for(let i=0;i<array.length;i++)
+    {
+        if(str.indexOf(array[i])!=-1)
+        {
+            for(let j=0;j<str.length;j++){
+                if(str[j]==array[i]) count++;
+            }
+        }
+    }
+    return count;
+
+    
+  }
+
+  const str ="abracadabraii";
+  console.log(getCount(str));
+
+  console.log(Math.round(2.3));
+
+  function getAverage(marks){
+
+       let sum=0;
+        for(let i=0;i<marks.length;i++)
+        {   
+            sum+=marks[i];
+        }
+        return Math.round(sum/marks.length);
+  }
+
+  var arr = [1,5,87,45,8,8];
+  console.log(getAverage(arr));
 
 
-console.log(personalMoviesDB);
-
+  var sex = [1,24,5,6,7,66,4,2,4];
+ let mx1=0,mx2=0;
+  for(let i=0;i<sex.length;i++)
+  {
+        if(sex[i]>mx1)
+        {
+            mx2 = mx1;
+            mx1 = sex[i];
+        }
+        
+  }
+  console.log(mx1,mx2);
