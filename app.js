523e408e2afb5f59ef0fb76f6b4a0ec0fd6cc7d8
Roman <troschkin.roma@yandex.ru>
2023-09-05 11:43:30 +0300

    second

--- app.js
+++
@@ -1,2 +0,0 @@
-let result = confirm("Are you here?");
-console.log(confirm);
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-//const result = confirm("Are you here?");
-//console.log(confirm);
+const numberOfFilms = prompt("How many movies do you see?","0");
 
-const some = prompt("do you have eighteen?");
-console.log(8 == 2*2+2);
+//console.log(numberOfFilms);
+
+const personalMoviesDB = {
+    count:  numberOfFilms,
+    movies: {},
+    geners: [],
+   privat: false
+};
+
+const lastmovies = prompt("One of the last movies","Name of movie");
+let ans1 = lastmovies;
+
+const rate = prompt("Give points rate 0 to 10","0");
+let ans2 = rate;
+
+personalMoviesDB.movies[0]=ans1;
+personalMoviesDB.movies[1]=ans2;
+
+console.log(personalMoviesDB);
+
